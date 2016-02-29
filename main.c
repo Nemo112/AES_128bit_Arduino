4a10b26afec58676e51add3c054e455ea7f7bf98
Nemo112 <nemo@nemor.cz>
2016-02-29 10:11:27 +0100

    Preparing for smartcard

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
-#define ROTL8(x,shift) ((byte) ((x) << (shift)) | ((x) >> (8 - (shift)))) // wiki
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+#define ROTL8(x,shift) ((unsigned char) ((x) << (shift)) | ((x) >> (8 - (shift)))) // wiki
 
 //
 void rotate(unsigned char *in) {
@@ -28,9 +33,9 @@ unsigned char rcon(unsigned char in) {
 }
 
 // wiki
-byte sbox(byte p_get) {
+unsigned char sbox(unsigned char p_get) {
   /* loop invariant: p * q == 1 in the Galois field */
-  byte p = 1, q = 1, tmp;
+  unsigned char p = 1, q = 1, tmp;
   do {
     /* multiply p by x+1 */
     p = p ^ (p << 1) ^ (p & 0x80 ? 0x1B : 0);
@@ -50,9 +55,9 @@ byte sbox(byte p_get) {
   return 0x63;
 }
 
-byte sbox_inv(byte p_get) {
+unsigned char sbox_inv(unsigned char p_get) {
   /* loop invariant: p * q == 1 in the Galois field */
-  byte p = 1, q = 1, tmp;
+  unsigned char p = 1, q = 1, tmp;
   do {
     /* multiply p by x+1 */
     p = p ^ (p << 1) ^ (p & 0x80 ? 0x1B : 0);
@@ -75,7 +80,7 @@ byte sbox_inv(byte p_get) {
 }
 
 /*
-void sbox_field(unsigned char * fild,unsigned char * inv_fild){  byte p = 1, q = 1;
+void sbox_field(unsigned char * fild,unsigned char * inv_fild){  unsigned char p = 1, q = 1;
   do {
     p = p ^ (p << 1) ^ (p & 0x80 ? 0x1B : 0);
     q ^= q << 1;
@@ -92,79 +97,92 @@ void sbox_field(unsigned char * fild,unsigned char * inv_fild){  byte p = 1, q =
 */
 
 
+void translate_matrix(unsigned char * mat){
+  for(unsigned char i = 0; i < 4 ;i ++)
+    for(unsigned char j = i; j < 4 ;j ++){
+      if (i != j){
+        unsigned char tmp = mat[i*4+j];
+        mat[i*4+j] = mat[j*4+i];
+        mat[j*4+i] = tmp;
+      }  
+   }
+}
+
+
 // My work! beranm14
-void printMatrix(unsigned char * wrd, byte x = 4, byte y = 4){
-  for(byte i=0; i<x; i++){
-    for(byte j=0; j<y; j++){   
-      Serial.print(((byte)wrd[(4*i)+j]),HEX);
-      Serial.print(" "); 
+void printMatrix(unsigned char * wrd, unsigned char x = 4, unsigned char y = 4){
+  for(unsigned char i=0; i<x; i++){
+    for(unsigned char j=0; j<y; j++){   
+      printf("%x",((unsigned char)wrd[(4*i)+j]));
+      printf(" "); 
     } 
-    Serial.print("\n");
+    printf("\n");
   }
 }
 /*
 void printExpKey(unsigned char * key){
-  byte i = 0;
+  unsigned char i = 0;
   while (i<176){
     if(i%16 == 0)
-      Serial.print("\n");
-    Serial.print(((byte)key[i]),HEX);
+      printf("\n");
+    printf(((unsigned char)key[i]),HEX);
     i++;
   }
-  Serial.print("\n");
+  printf("\n");
 }
 
 
 void printSbox(){
-  for(byte y = 0; y < 16 ;y++){
-    for(byte x = 0; x < 16 ;x++){
-      Serial.print( sbox( ((y << 4 | x)) ),HEX);
-      Serial.print(" ");
-      Serial.print((y << 4 | x));
-      Serial.print("|");
+  for(unsigned char y = 0; y < 16 ;y++){
+    for(unsigned char x = 0; x < 16 ;x++){
+      printf( sbox( ((y << 4 | x)) ),HEX);
+      printf(" ");
+      printf((y << 4 | x));
+      printf("|");
     }
-    Serial.print("\n");
+    printf("\n");
   }
 }
 void printInvSbox(){
-  for(byte y = 0; y < 16 ;y++){
-    for(byte x = 0; x < 16 ;x++){
-      //Serial.print(sbox_inv( (y << 4 | x) ),HEX);
-      //Serial.print(" ");
-      Serial.print( sbox_inv( (y << 4 | x) ),HEX);
-      Serial.print(" ");
-      Serial.print((y << 4 | x));
-      Serial.print("|");
+  for(unsigned char y = 0; y < 16 ;y++){
+    for(unsigned char x = 0; x < 16 ;x++){
+      //printf(sbox_inv( (y << 4 | x) ),HEX);
+      //printf(" ");
+      printf( sbox_inv( (y << 4 | x) ),HEX);
+      printf(" ");
+      printf((y << 4 | x));
+      printf("|");
     }
-    Serial.print("\n");
+    printf("\n");
   }
 }
 */
 
 // My work! beranm14
 void ShiftRows(unsigned char * wrd,unsigned char * tmp){
-  translate_matrix(wrd);
-  for(byte i=0; i<4; i++){
-    for(byte j=0; j<4; j++){
-      tmp[(4*j)+i] = wrd[(4*j)+(j+i)%4];   
+  //translate_matrix(wrd);
+  for(unsigned char i=0; i<4; i++){
+    for(unsigned char j=0; j<4; j++){
+      tmp[(4*i)+j] = wrd[(4*((j+i)%4))+j];   
     } 
   }
- for(byte i=0; i<16; i++){
+ for(unsigned char i=0; i<16; i++){
     wrd[i] = tmp[i]; 
  }
- translate_matrix(wrd);
+ //translate_matrix(wrd);
 }
 void InvShiftRows(unsigned char * wrd,unsigned char * tmp){
- translate_matrix(wrd);
- for(byte i=0; i<4; i++){
-    for(byte j=0; j<4; j++){
-      tmp[(4*j)+(j+i)%4] = wrd[(4*j)+i];   
+ //translate_matrix(wrd);
+ for(unsigned char i=0; i<4; i++){
+    for(unsigned char j=0; j<4; j++){
+      //tmp[(4*j)+(j+i)%4] = wrd[(4*j)+i];   
+      tmp[(4*((j+i)%4))+j] = wrd[(4*i)+j];   
     } 
   }
- for(byte i=0; i<16; i++){
+ for(unsigned char i=0; i<16; i++){
     wrd[i] = tmp[i]; 
  }
- translate_matrix(wrd);
+ //translate_matrix(wrd);
 }
 
 // Wiki
@@ -190,15 +208,15 @@ void MixFour(unsigned char * wrd){
         wrd[3] = b[3] ^ a[2] ^ a[1] ^ b[0] ^ a[0]; /* 2 * a3 + a2 + a1 + 3 * a0 */
 }
 
-byte gmul(byte a, byte b) {
-   byte p = 0;
-   byte counter;
-   byte hi_bit_set;
+unsigned char gmul(unsigned char a, unsigned char b) {
+   unsigned char p = 0;
+   unsigned char counter;
+   unsigned char hi_bit_set;
    for (counter = 0; counter < 8; counter++) {
       if ((b & 1) != 0) {
          p ^= a;
       }
-      hi_bit_set = (byte) (a & 0x80);
+      hi_bit_set = (unsigned char) (a & 0x80);
       a <<= 1;
       if (hi_bit_set != 0) {
          a ^= 0x1b; /* x^8 + x^4 + x^3 + x + 1 */
@@ -214,36 +232,38 @@ void InvMixFour(unsigned char * s){
      ss[1] = (gmul(0x09, s[0]) ^ gmul(0x0e, s[1]) ^ gmul(0x0b,s[2]) ^ gmul(0x0d,s[3]));
      ss[2] = (gmul(0x0d, s[0]) ^ gmul(0x09, s[1]) ^ gmul(0x0e,s[2]) ^ gmul(0x0b,s[3]));
      ss[3] = (gmul(0x0b, s[0]) ^ gmul(0x0d, s[1]) ^ gmul(0x09,s[2]) ^ gmul(0x0e,s[3]));
-     for(byte i = 0;i<4;i++)
+     for(unsigned char i = 0;i<4;i++)
         s[i] = ss[i];
 }
+
 void MixColumns(unsigned char * wrd){
-   translate_matrix(wrd);
+   //translate_matrix(wrd);
     unsigned char tmp[4];
-    for (byte i = 0; i<4; i++){
-      for (byte j = 0; j<4; j++){
-        tmp[j] = wrd[j*4+i];
+    for (unsigned char i = 0; i<4; i++){
+      for (unsigned char j = 0; j<4; j++){
+        tmp[j] = wrd[i*4+j];
       }
       MixFour(tmp);
-      for (byte j = 0; j<4; j++){
-        wrd[j*4+i] = tmp[j];
+      for (unsigned char j = 0; j<4; j++){
+        wrd[i*4+j] = tmp[j];
       }
     }
-    translate_matrix(wrd);
+    //translate_matrix(wrd);
 }
+
 void InvMixColumns(unsigned char * wrd){
-   translate_matrix(wrd);
+   //translate_matrix(wrd);
     unsigned char tmp[4];
-    for (byte i = 0; i<4; i++){
-      for (byte j = 0; j<4; j++){
-        tmp[j] = wrd[j*4+i];
+    for (unsigned char i = 0; i<4; i++){
+      for (unsigned char j = 0; j<4; j++){
+        tmp[j] = wrd[i*4+j];
       }
       InvMixFour(tmp);
-      for (byte j = 0; j<4; j++){
-        wrd[j*4+i] = tmp[j];
+      for (unsigned char j = 0; j<4; j++){
+        wrd[i*4+j] = tmp[j];
       }
     }
-   translate_matrix(wrd);
+   //translate_matrix(wrd);
 }
 
 //http://www.samiam.org/key-schedule.html
@@ -251,27 +271,27 @@ void schedule_core(unsigned char *in, unsigned char i) {
         char a;
         /* Rotate the input 8 bits to the left */
         rotate(in);
-        /* Apply Rijndael's s-box on all 4 bytes */
+        /* Apply Rijndael's s-box on all 4 unsigned chars */
         for(a = 0; a < 4; a++) 
                 in[a] = sbox(in[a]);
-        /* On just the first byte, add 2^i to the byte */
+        /* On just the first unsigned char, add 2^i to the unsigned char */
         in[0] ^= rcon(i);
 }
 
 //http://www.samiam.org/key-schedule.html
-void expand_key(unsigned char *in) { //128 bit to another 176 bytes
+void expand_key(unsigned char *in) { //128 bit to another 176 unsigned chars
         unsigned char t[4];
         /* c is 16 because the first sub-key is the user-supplied key */
         unsigned char c = 16;
         unsigned char i = 1;
         unsigned char a;
-        /* We need 11 sets of sixteen bytes each for 128-bit mode */
+        /* We need 11 sets of sixteen unsigned chars each for 128-bit mode */
         while(c < 176) {
-                /* Copy the temporary variable over from the last 4-byte
+                /* Copy the temporary variable over from the last 4-unsigned char
                  * block */
                 for(a = 0; a < 4; a++) 
                         t[a] = in[a + c - 4];
-                /* Every four blocks (of four bytes), 
+                /* Every four blocks (of four unsigned chars), 
                  * do a complex calculation */
                 if(c % 16 == 0) {
                     schedule_core(t,i);
@@ -285,25 +305,25 @@ void expand_key(unsigned char *in) { //128 bit to another 176 bytes
 }
 
 void addRoundKey(unsigned char * key, unsigned char * wrd){
-  for(byte i=0;i<16;i++){
+  for(unsigned char i=0;i<16;i++){
     wrd[i] = wrd[i] ^ key[i];
   }
 }
 
-void InvSubsByte(unsigned char * wrd){
-  for (byte i = 0;i<16;i++){
-        byte x = wrd[i] & 15;
-        byte y = wrd[i] >> 4 & 15;
-        byte gtxy = y*16+x;
+void InvSubByte(unsigned char * wrd){
+  for (unsigned char i = 0;i<16;i++){
+        unsigned char x = wrd[i] & 15;
+        unsigned char y = wrd[i] >> 4 & 15;
+        unsigned char gtxy = y*16+x;
         wrd[i] = sbox_inv(gtxy);
   }  
 }
 
-void SubsByte(unsigned char * wrd){
-  for (byte i = 0;i<16;i++){
-        byte x = wrd[i] & 15;
-        byte y = wrd[i] >> 4 & 15;
-        byte gtxy = y*16+x;
+void SubByte(unsigned char * wrd){
+  for (unsigned char i = 0;i<16;i++){
+        unsigned char x = wrd[i] & 15;
+        unsigned char y = wrd[i] >> 4 & 15;
+        unsigned char gtxy = y*16+x;
         wrd[i] = sbox(gtxy);
   }  
 }
@@ -314,9 +334,9 @@ unsigned char * AES_encrypt_128(unsigned char * wrd, unsigned char * key, unsign
     addRoundKey(key, wrd);
     key+=16;
     // rounds
-    for(byte i = 0; i<9; i++){
-      // sub bytes
-      SubsByte(wrd);
+    for(unsigned char i = 0; i<9; i++){
+      // sub unsigned chars
+      SubByte(wrd);
       // shift rows
       ShiftRows(wrd,tmp);
       // mix columns
@@ -326,8 +346,8 @@ unsigned char * AES_encrypt_128(unsigned char * wrd, unsigned char * key, unsign
       key+=16;
     }
     // final round
-    // sub bytes
-    SubsByte(wrd);
+    // sub unsigned chars
+    SubByte(wrd);
     // shift rows  
     ShiftRows(wrd,tmp);
     // mix with keys
@@ -343,92 +363,37 @@ unsigned char * AES_decrypt_128(unsigned char * wrd, unsigned char * key, unsign
     addRoundKey(key, wrd);
     key -= 16;
     // rounds
-    for(byte i = 0; i<9; i++){
+    for(unsigned char i = 0; i<9; i++){
       InvShiftRows(wrd,tmp);
-      InvSubsByte(wrd);
+      InvSubByte(wrd);
       addRoundKey(key, wrd);
       key -= 16;     
       InvMixColumns(wrd);
     }
     // final round
     InvShiftRows(wrd,tmp);
-    InvSubsByte(wrd);
+    InvSubByte(wrd);
     addRoundKey(key, wrd);
     return wrd;
 }
 
 
-void setup() {
-  Serial.begin(9600);
-}
-
-void translate_matrix(unsigned char * mat){
-  for(byte i = 0; i < 4 ;i ++)
-    for(byte j = i; j < 4 ;j ++){
-      if (i != j){
-        byte tmp = mat[i*4+j];
-        mat[i*4+j] = mat[j*4+i];
-        mat[j*4+i] = tmp;
-      }  
-   }
-}
-  
-void loop() {
-  //unsigned char  key[176] = { 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-  //unsigned char  key[176] = { 219,19,83,69,   242, 10, 34, 92,   1,1,1,1,    198, 198, 198, 198 };
-  //0102030405060708090a0b0c0d0e0f10
-  unsigned char  key[176] = { 0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,0x10};
-  unsigned char  wrd[16] = { 0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,0x10};
+int main(int argc, char *argv[]){
+  unsigned char  key[176] = { 0x01,0x02,0x03,0x04,0x05,0xA0,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,0x10};
+  unsigned char  wrd[16] = { 0x01,0x02,0x03,0x04,0x05,0xFF,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,0x10};
   unsigned char  tmp[16];
-  //unsigned char  tmp[4] = {219, 19, 83, 69};
-  //expand_key(key);
-  //printExpKey(key);
-  //Serial.print("\n");
-  //expand_key(key);
-  //printExpKey(key);
-  //Serial.print("\n");
-  /*for(byte i = 0; i<4 ;i++){
-    Serial.print(tmp[i]);
-    Serial.print(" ");
-  }
-  Serial.print("\n");
-  MixFour(tmp);
-  for(byte i = 0; i<4 ;i++){
-    Serial.print(tmp[i]);
-    Serial.print(" ");
-  }
-  Serial.print("\n");
-  InvMixFour(tmp);
-  for(byte i = 0; i<4 ;i++){
-    Serial.print(tmp[i]);
-    Serial.print(" ");
-  }
-  Serial.print("\n");*/
-  //sbox_field(fild,inv_fild);
-
-  /*printSbox();
-  Serial.print("\n");
-  printInvSbox();
-  Serial.print("\n");*/
-/*  delay(1000000);
-  ShiftRows(wrd,tmp);
-  Serial.print("\n");
+  printf("Vstup:\n");
   printMatrix(wrd);
-  Serial.print("\n");
-  delay(100000);*/
-  Serial.print("\n");
-  printMatrix(wrd);
-  Serial.print("\n\n");
-  //translate_matrix(key);
-  //translate_matrix(wrd);
   AES_encrypt_128(wrd, key, tmp);
-  Serial.print("\n");
+  printf("\nEncrypted\n");
   printMatrix(wrd);
-  Serial.print("\n\n");
-  Serial.print("\n\n");
+  printf("\n");
   AES_decrypt_128(wrd, key, tmp);
-  Serial.print("\n");
+  printf("Decrypted\n");
   printMatrix(wrd);
-  Serial.print("\n\n");
-  delay(1000000);
+  printf("\n\n");
+  return 0;
 }
+
+
+
